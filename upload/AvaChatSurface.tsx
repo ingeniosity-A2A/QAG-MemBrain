792d94591b60ab1577a576c1b191a480f57714b7
Z User <z@container>
2026-06-13 19:44:07 +0000

    9cf53e93-81c6-42f4-a8e6-34a8b22827e2

---
+++ upload/AvaChatSurface.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AvaChatSurface() {
+  return (
+    <div>
+      AvaChatSurface
+    </div>
+  );
+}
---
+++ upload/TrainModelHeader.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function TrainModelHeader() {
+  return (
+    <div>
+      TrainModelHeader
+    </div>
+  );
+}
